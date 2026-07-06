0ca042995f89fc158420eb4a71e2a63843498140
IsaiasGs-prog <isaiassilvestri555@gmail.com>
2026-07-06 20:24:47 -0300

    Añade archivo datos.py con información de clientes y empleados; crea principal.py para imprimir datos de clientes.

---
+++ modulo_ejercicio/principal.py
@@ -0,0 +1,7 @@
+import datos
+
+print(datos.base_datos["clientes"][0]["Nombre"])
+print(datos.base_datos["clientes"][0]["Apellido"])
+print(datos.base_datos["clientes"][0]["Direccion"])
+print(datos.base_datos["clientes"][0]["Telefono"])
+print(datos.base_datos["clientes"][0]["Email"])
